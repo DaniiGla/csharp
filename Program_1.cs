f8b549cb61cebf58120afe5c053d633662d335d6
DaniiGla <108856897+DaniiGla@users.noreply.github.com>
2024-07-04 02:36:54 +0500

    Add files via upload

---
+++ Program_1.cs
@@ -0,0 +1,41 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        IntegersBetween(9, -600);
+    }
+    static void IntegersBetween(int m, int n)
+    {
+        if (m < n)
+        {
+            if (m - 1 <= n)
+            {
+                if (n > m + 1)
+                {
+                    IntegersBetween(m, n - 1);
+                    if (n > 1)
+                    {
+                        Console.Write(n - 1 + " ");
+                    }
+                }
+            }
+        }
+
+        else if (m > n + 1)
+        {
+            IntegersBetween(n, m - 1);
+            if (m > 1)
+            {
+                Console.Write(m - 1 + " ");
+            }
+        }
+    }
+
+}
+
+
+
+
